d71a663d2cabf43b9dc6d51aac6b9de00352f640
Ujjval Soni <32246771+ujjvalsoni@users.noreply.github.com>
2018-02-23 14:39:29 -0800

    mouse events on buttons

---
+++ public/userinteractivity.js
@@ -0,0 +1,59 @@
+function mouseOver() {
+    document.getElementById("b1").style.background = "green";
+    //document.getElementById("b2").style.background = "green";
+}
+
+function mouseOut() {
+    document.getElementById("b1").style.background ='#FF0000';
+    //document.getElementById("b2").style.background ='#FF0000';
+}
+
+function mouseOver1() {
+    //document.getElementById("b1").style.background = "green";
+    document.getElementById("b2").style.background = "green";
+}
+
+function mouseOut1() {
+    //document.getElementById("b1").style.background ='#FF0000';
+    document.getElementById("b2").style.background ='#FF0000';
+}
+
+function mouseOver2() {
+    //document.getElementById("b1").style.background = "green";
+    document.getElementById("b3").style.background = "green";
+}
+
+function mouseOut2() {
+    //document.getElementById("b1").style.background ='#FF0000';
+    document.getElementById("b3").style.background ='#FF0000';
+}
+
+function mouseOver3() {
+    //document.getElementById("b1").style.background = "green";
+    document.getElementById("b4").style.background = "green";
+}
+
+function mouseOut3() {
+    //document.getElementById("b1").style.background ='#FF0000';
+    document.getElementById("b4").style.background ='#FF0000';
+}
+
+function mouseOver4() {
+    //document.getElementById("b1").style.background = "green";
+    document.getElementById("b5").style.background = "green";
+}
+
+function mouseOut4() {
+    //document.getElementById("b1").style.background ='#FF0000';
+    document.getElementById("b5").style.background ='#FF0000';
+}
+
+function mouseOver5() {
+    //document.getElementById("b1").style.background = "green";
+    document.getElementById("b6").style.background = "green";
+}
+
+function mouseOut5() {
+    //document.getElementById("b1").style.background ='#FF0000';
+    document.getElementById("b6").style.background ='#FF0000';
+}
