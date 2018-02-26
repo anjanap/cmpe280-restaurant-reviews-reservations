c08fd522912ce97143e1b73cdc3472e8aa5dbdac
Ujjval Soni <32246771+ujjvalsoni@users.noreply.github.com>
2018-02-26 03:01:00 -0800

    hamburger

--- public/modal.js
+++ public/modal.js
@@ -11,3 +11,15 @@ var signin = document.getElementById('id02');
             signup.style.display = "none";
         }
       }
+
+
+function w3_open() {
+    if(document.getElementById("mySidebar").style.display == "none")
+        document.getElementById("mySidebar").style.display = "block";
+    else
+        document.getElementById("mySidebar").style.display = "none";
+}
+
+function loadfirst() {
+    document.getElementById("mySidebar").style.display = "none";
+}
