df733231b5b29b676673a21a972ce1cc0c1e1045
Prathmesh Parmar <prathmesh272@outlook.com>
2018-04-09 16:21:34 -0700

    Mongoose

--- models/model_review.js
+++ models/model_review.js
@@ -4,6 +4,6 @@ var Schema = mongoose.Schema;
 var reviewSchema = new Schema({
     userid: Number,
     review: { type: String, required: true}
-});
+},{collection:'reviews'});
 
 module.exports = mongoose.model('review', reviewSchema);
--- routes/reviews.js
+++ routes/reviews.js
@@ -2,11 +2,10 @@
 //var url = "mongodb://localhost:27017/cmpe280";
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost:27017/cmpe280');
-var review = require('../models/model_review.js');
+var review_model = require('../models/model_review.js');
 var ObjectId = require('mongodb').ObjectID;
 
 function add(req,res){
-    
     var review = req.body.newreview;
     var userid = 1;
     console.log(review);
@@ -14,9 +13,10 @@ function add(req,res){
         userid: 1,
         review: review
     };
-    var newReview = new review(item);
+    console.log(item);
+    var newReview = new review_model(item);
     console.log(newReview + "HHHHHH") ;
-    var flag = newUser.save();
+    var flag = newReview.save();
     res.render('../views/success.html');
 }
 // function add(req, res) {
@@ -41,70 +41,111 @@ function add(req,res){
 // }//function
 exports.add = add;
 
-
-function display(req, res) {
-
-    var userid = 1;
-    console.log("user: " + userid);
-
-    MongoClient.connect(url, function(err, db) {
-        if (err) throw err;
-        var dbo = db.db("cmpe280");
-        dbo.collection("reviews").find({userid: userid}).toArray(function(err, result) {
-            if (err) throw err;
-            if(result)
-                res.send(result);
-            else
-                res.render('../views/error.html');
-            db.close();
-        });
+function display(req,res){
+    var userid = 1;    
+    console.log(userid + "&&&&&&&");
+    review_model.find({userid: userid},function(err,result){
+        if(err) throw err;
+        if(result){
+            console.log("inside if");
+            res.send(result);
+        }
+        else
+            res.render('../views/error.html');
     });
+}
+// function display(req, res) {
 
-}//function
+//     var userid = 1;
+//     console.log("user: " + userid);
+
+//     MongoClient.connect(url, function(err, db) {
+//         if (err) throw err;
+//         var dbo = db.db("cmpe280");
+//         dbo.collection("reviews").find({userid: userid}).toArray(function(err, result) {
+//             if (err) throw err;
+//             if(result)
+//                 res.send(result);
+//             else
+//                 res.render('../views/error.html');
+//             db.close();
+//         });
+//     });
+
+// }//function
 exports.display = display;
 
-
-function update(req, res) {
+function update(req,res){
 
     var id = req.body.updateid;
     var review = req.body.updatereview;
 
-    MongoClient.connect(url, function(err, db) {
-        if (err) throw err;
-        var dbo = db.db("cmpe280");
-        var myquery = {_id :ObjectId(id)};
-        var newvalues = { $set: { review: review} };
-        dbo.collection("reviews").updateOne(myquery, newvalues, function(err, result) {
-            if (err) throw err;
-            if(result)
-                res.render('../views/success.html');
-            else
-                res.render('../views/error.html');
-            db.close();
-        });
+    var myquery = {_id :ObjectId(id)};
+    var newvalues = { $set: { review: review} };
+
+    review_model.updateOne(myquery,newvalues,function(err,result){
+        if(err) throw err;
+        if(result)
+            res.render('../views/success.html');
+        else
+            res.render('../views/error.html');
     });
 
-}//function
+}
+
+
+// function update(req, res) {
+
+//     var id = req.body.updateid;
+//     var review = req.body.updatereview;
+
+//     MongoClient.connect(url, function(err, db) {
+//         if (err) throw err;
+//         var dbo = db.db("cmpe280");
+//         var myquery = {_id :ObjectId(id)};
+//         var newvalues = { $set: { review: review} };
+//         dbo.collection("reviews").updateOne(myquery, newvalues, function(err, result) {
+//             if (err) throw err;
+//             if(result)
+//                 res.render('../views/success.html');
+//             else
+//                 res.render('../views/error.html');
+//             db.close();
+//         });
+//     });
+
+// }//function
 exports.update = update;
 
-
-function remove(req, res) {
-
+function remove(req,res){
     var id = req.param("id");//'5ac91d3f0683426258aa387c';
-console.log(id);
-    MongoClient.connect(url, function(err, db) {
-        if (err) throw err;
-        var dbo = db.db("cmpe280");
-        var myquery = {_id :ObjectId(id)};
-        dbo.collection("reviews").deleteOne(myquery, function(err, result) {
-            if (err) throw err;
-            if(result)
-                res.send({result:1});
-            else
-                res.render('../views/error.html');
-            db.close();
-        });
+    console.log(id + "++++++++++++==");
+    var myquery = {_id :ObjectId(id)};
+    review_model.deleteOne(myquery,function(err,result){
+    if(err) throw err;
+    if(result)
+        res.send({result:1});
+    else
+        res.render('../views/error.html');
     });
+}
+// function remove(req, res) {
 
-}//function
+//     var id = req.param("id");//'5ac91d3f0683426258aa387c';
+// console.log(id);
+//     MongoClient.connect(url, function(err, db) {
+//         if (err) throw err;
+//         var dbo = db.db("cmpe280");
+//         var myquery = {_id :ObjectId(id)};
+//         dbo.collection("reviews").deleteOne(myquery, function(err, result) {
+//             if (err) throw err;
+//             if(result)
+//                 res.send({result:1});
+//             else
+//                 res.render('../views/error.html');
+//             db.close();
+//         });
+//     });
+
+// }//function
 exports.remove = remove;
