7f71574b36c952f152439f2f64c221bd35fec614
Anjana Pradeep <32275066+anjanapradeep13@users.noreply.github.com>
2018-04-28 17:33:28 -0700

    restaurant spelling

--- routes/search.js
+++ routes/search.js
@@ -4,22 +4,23 @@ var reviews = require('./reviews');
 
 function search(req, res) {
 
-    var resturant_name = req.body.resturant_name;
+    var restaurant_name = req.body.restaurant_name;
+    console.log(restaurant_name);
     var u_id = 1;
     var final = [];
 
     MongoClient.connect(url, function(err, db) {
         if (err) throw err;
         var dbo = db.db("cmpe280");
-        dbo.collection("resturant_details").find({ resturant_name: resturant_name}).toArray(function(err, r_result) {
+        dbo.collection("restaurant_details").find({ restaurant_name: restaurant_name}).toArray(function(err, r_result) {
             if (err) throw err;
             if(r_result.length == 0)
-                res.send("No Resturants found!");
+                res.send("No restaurants found!");
             else {
                 var r_id = r_result[0]._id + "";
                 console.log(r_id);
                 //r_id = "5ae4d0f8d102c4c87b48c5cc";
-                dbo.collection("reviews").find({userid: u_id, resturantid: r_id}).toArray(function(err, result) {
+                dbo.collection("reviews").find({userid: u_id, restaurantid: r_id}).toArray(function(err, result) {
                     if (err) throw err;
                     if (result.length == 0)
                     {
