32e45553e0e0a144c08ef9db578a980d511461df
Anjana Pradeep <32275066+anjanapradeep13@users.noreply.github.com>
2018-05-03 01:34:06 -0700

    css changes

--- public/js/homejs.js
+++ public/js/homejs.js
@@ -1,12 +1,56 @@
+/* JS file for home.html */
+
+//Ajax call for getting all restaurants.
+$(document).ready(function() {
+    var resultarr='';
+    var resultarr1='';
+    $.ajax({
+        url: 'http://localhost:3000/getrestaurants',
+        type: 'GET',
+        success: function (data) {
+            for(var i = 0; i<data.length/2; i++){
+                var abc = "abc"
+                resultarr+='<div class="featured-place-wrap">';
+                resultarr+="<button onclick='MyAjaxFunction(\""+ data[i].restaurant_name + "\");'>";
+                resultarr+='<img src='+data[i].img+' class="img-fluid" alt="#">';
+                resultarr+='<span class="featured-rating-green">' + data[i].rating + '</span>';
+                resultarr+='<div class="featured-title-box">';
+                resultarr+='<h6>' + data[i].restaurant_name + '</h6>';
+                resultarr+='<p>' + data[i].Address+ '</p>';
+                resultarr+='</div>';
+                resultarr+='</button>';
+                resultarr+='</div>';
+            }
+
+            for(var i = data.length/2; i<data.length; i++){
+                resultarr1+='<div class="featured-place-wrap">';
+                resultarr1+="<button onclick='MyAjaxFunction(\""+ data[i].restaurant_name + "\");'>";
+                resultarr1+='<img src='+data[i].img+' class="img-fluid" alt="#">';
+                resultarr1+='<span class="featured-rating-green">' + data[i].rating + '</span>';
+                resultarr1+='<div class="featured-title-box">';
+                resultarr1+='<h6>' + data[i].restaurant_name + '</h6>';
+                resultarr1+='<p>' + data[i].Address+ '</p>';
+                resultarr1+='</div>';
+                resultarr1+='</button>';
+                resultarr1+='</div>';
+            }
+
+            document.getElementById("display").innerHTML=resultarr;
+            document.getElementById("display1").innerHTML=resultarr1;
+        },
+        error: function (xhr, status, error) {
+            console.log('Error: ' + error.message);
+        },
+    });
+    event.preventDefault();
+});//Ajax call for getting all restaurants.
+
+
 //Button Search Ajax call
 function MyAjaxFunction(a){
     var showtab=document.getElementById("displaylist");
     showtab.style.display="none";
-
-    var formData = {
-        'restaurant_name': a
-    };
-
+    var formData = {'restaurant_name': a};
     $.ajax({
         url: 'http://localhost:3000/search',
         data: formData,
@@ -23,7 +67,6 @@ function MyAjaxFunction(a){
             resultarr2+='   <p class="reserve-description">'+data[0].Cuisine+'</p>';
             resultarr2+='   </div>';
             resultarr2+='           </div></div></section>';
-
             resultarr2+='   <section class="light-bg booking-details_wrap">';
             resultarr2+='   <div class="container">';
             resultarr2+='<div class="row">';
@@ -34,18 +77,17 @@ function MyAjaxFunction(a){
 
             for(var j=1;j<data.length;j++){
                 resultarr2+='        <div class="customer-review_wrap">';
-
                 resultarr2+='     <p>'+data[j].uname+'</p><p class="customer-text">'+data[j].Description+'</p>'
                 resultarr2+='      </div>';
                 resultarr2+='        <hr>';
             }
             resultarr2+='      </div>';
             resultarr2+='  </div>';
-            resultarr2+='  <div class="col-md-4 responsive-wrap"><div class="contact-info">';
+            resultarr2+='  <div class="col-md-4 responsive-wrap">';
             resultarr2+='        <div class="address">';
             resultarr2+='          <b><p>Address</p></b>';
             resultarr2+='          <p> '+ data[0].Address+'</p>';
-            resultarr2+='    </div></div></div>';
+            resultarr2+='    </div></div>';
             resultarr2+='  </div>';
             resultarr2+='</div>';
             resultarr2+='</section>';
@@ -61,68 +103,6 @@ function MyAjaxFunction(a){
         },
     });
 }//Button Search Ajax call
-
-
-//Ajax call for getting all restaurants.
-$(document).ready(function() {
-    var resultarr='';
-    var resultarr1='';
-    $.ajax({
-        url: 'http://localhost:3000/getrestaurants',
-        type: 'GET',
-        success: function (data) {
-            temp=data;
-            console.log(data[0].restaurant_name);
-
-
-            for(var i = 0; i<data.length/2; i++){
-                var abc = "abc"
-                resultarr+='<div class="featured-place-wrap">';
-                resultarr+="<button onclick='MyAjaxFunction(\""+ data[i].restaurant_name + "\");'>";
-                resultarr+='<img src='+data[i].img+' class="img-fluid" alt="#">';
-                resultarr+='<span class="featured-rating-green">' + data[i].rating + '</span>';
-                resultarr+='<div class="featured-title-box">';
-                resultarr+='<h6>' + data[i].restaurant_name + '</h6>';
-
-                resultarr+='<ul>';
-
-                resultarr+='<li><p>' + data[i].Address+ '</p></li>';
-
-                resultarr+='</ul>';
-                resultarr+='</div>';
-                resultarr+='</button>';
-                resultarr+='</div>';
-            }
-
-            for(var i = data.length/2; i<data.length; i++){
-                resultarr1+='<div class="featured-place-wrap">';
-                resultarr1+="<button onclick='MyAjaxFunction(\""+ data[i].restaurant_name + "\");'>";
-                resultarr1+='<img src='+data[i].img+' class="img-fluid" alt="#">';
-                resultarr1+='<span class="featured-rating-green">' + data[i].rating + '</span>';
-                resultarr1+='<div class="featured-title-box">';
-                resultarr1+='<h6>' + data[i].restaurant_name + '</h6>';
-
-                resultarr1+='<ul>';
-
-                resultarr1+='<li><p>' + data[i].Address+ '</p></li>';
-
-                resultarr1+='</ul>';
-                resultarr1+='</div>';
-                resultarr1+='</button>';
-                resultarr1+='</div>';
-            }
-
-            document.getElementById("display").innerHTML=resultarr;
-            document.getElementById("display1").innerHTML=resultarr1;
-
-        },
-        error: function (xhr, status, error) {
-            console.log('Error: ' + error.message);
-        },
-    });
-    event.preventDefault();
-});//Ajax call for getting all restaurants.
-
 
 
 //Search bar search ajax call
@@ -175,11 +155,11 @@ $(document).ready(function() {
                         }
                         resultarr2+='      </div>';
                         resultarr2+='  </div>';
-                        resultarr2+='  <div class="col-md-4 responsive-wrap"><div class="contact-info">';
+                        resultarr2+='  <div class="col-md-4 responsive-wrap">';
                         resultarr2+='        <div class="address">';
                         resultarr2+='          <b><p>Address</p></b>';
                         resultarr2+='          <p> '+ data[0].Address+'</p>';
-                        resultarr2+='    </div></div></div>';
+                        resultarr2+='    </div></div>';
                         resultarr2+='  </div>';
                         resultarr2+='</div>';
                         resultarr2+='</section>';
@@ -187,11 +167,8 @@ $(document).ready(function() {
 
                         document.getElementById("display2").innerHTML=resultarr2;
                         document.getElementById("display3").innerHTML=resultarr3;
-
-
                         var showtab=document.getElementById("tabs-1");
                         showtab.style.display="block";
-
                     }},
                 error: function (xhr, status, error) {
                     console.log('Error: ' + error.message);
@@ -204,7 +181,6 @@ $(document).ready(function() {
 
 //Signout Ajax call
 function  signout() {
-
     {
         $.ajax({
             url: 'http://localhost:3000/signout',
@@ -217,9 +193,7 @@ function  signout() {
                 console.log('Error: ' + error.message);
             }
         });
-    }}
-//Signout Ajax call
-
+    }}//Signout Ajax call
 
 
 //Ajax tabs
@@ -228,19 +202,14 @@ $(function() {
 });//Ajax tabs
 
 
-
 //search button disabled if search bar is empty
-
 $(document).ready(function() {
-
     $(function() {
         $('#searchbtn').attr('disabled', 'disabled');
     });
 
     $('input[type=text]').keyup(function() {
-
         if ($('#searchtxt').val()) {
-
             $('#searchbtn').removeAttr('disabled');
         } else {
             $('#searchbtn').attr('disabled', 'disabled');
